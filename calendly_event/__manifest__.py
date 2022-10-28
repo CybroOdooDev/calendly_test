db80c4f0c0b124df0c0d5cc862c7eb4e77011ae1
Cybrosys <cybroodoo@gmail.com>
2022-10-28 15:28:17 +0530

    [UPDT] changed wizard

--- calendly_event/__manifest__.py
+++ calendly_event/__manifest__.py
@@ -21,7 +21,7 @@
 #############################################################################
 {
     'name': 'Calendly Integration',
-    'version': '15.0.1.0.1',
+    'version': '1.0',
     'summary': 'Calendly Integration',
     'author': 'Cybrosys Techno solutions',
     'company': 'Cybrosys Techno Solutions',
